095076c6731b533149833111099ed57c7ae00462
sergei.bardas <sergei.bardas@cb2d3c85-9e30-58f3-8e0a-dd9ac8f198df>
2011-12-22 11:44:48 +0000

    Issue 5,Issue 6:.xml config for Spring Security, login page is initial

---
+++ src/main/java/main/LoginBean.java
@@ -0,0 +1,52 @@
+package main;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean {
+
+	private boolean isAuthenticated;
+
+	@ManagedProperty(value = "#{userDetails}")
+	private UserDetails userDetails;
+
+	public void setUserDetails(UserDetails userDetails) {
+		this.userDetails = userDetails;
+	}
+
+	public UserDetails getUserDetails() {
+		return userDetails;
+	}
+
+	@ManagedProperty(value = "#{authenticationService}")
+	private transient AuthenticationService authenticationService;
+
+	public void setAuthenticationService(AuthenticationService authenticationService) {
+		this.authenticationService = authenticationService;
+	}
+
+	public String doLogin() {
+		boolean isLoggedIn = authenticationService.login(userDetails.getUsername(), userDetails.getPassword());
+		if (isLoggedIn) {
+			isAuthenticated = true;
+			return "index";
+		}
+		FacesContext.getCurrentInstance().addMessage("login failure", new FacesMessage());
+		return "failureLogin";
+
+	}
+
+	public boolean isAuthenticated() {
+		return isAuthenticated;
+	}
+
+	public void setAuthenticated(boolean isAuthenticated) {
+		this.isAuthenticated = isAuthenticated;
+	}
+
+}
