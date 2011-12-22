2955aeffdbc26f5cd592680f5d9b5edb34aa288a
sergei.bardas <sergei.bardas@cb2d3c85-9e30-58f3-8e0a-dd9ac8f198df>
2011-12-22 10:38:50 +0000

    Issue 3:tables for Spring Security,Person and Employee info added to the script

--- trunk/src/main/webapp/resources/init-script/init.sql
+++ trunk/src/main/webapp/resources/init-script/init.sql
@@ -1,16 +1,48 @@
 SET FOREIGN_KEY_CHECKS = 0;
 drop table if exists decane;
+drop table if exists authorities;
+drop table if exists users;
+drop table if exists employee;
+drop table if exists person;
 SET FOREIGN_KEY_CHECKS = 1;
 
-CREATE TABLE `decane` (
-  `id` varchar(20) NOT NULL,
-  `first_name` varchar(30) NOT NULL DEFAULT '',
-  `last_name` varchar(30) NOT NULL DEFAULT '',
-  `faculty` varchar(50) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`)
+create table users(
+      username varchar(50) not null primary key,
+      password varchar(50) not null,
+      enabled boolean not null);
+      
+create table authorities (
+      username varchar(50) not null,
+      authority varchar(50) not null,
+      constraint fk_authorities_users foreign key(username) references users(username));
+      create unique index ix_auth_username on authorities (username,authority);
+      
+     
+create table employee (  
+        emp_id varchar(50) not null,
+        faculty varchar(50) not null,
+        state varchar(50) not null, 
+        constraint fk_person foreign key(emp_id) references users(username)
+);
+
+create table person (
+		pers_id varchar(50) not null,
+		dob date not null,
+		firstname varchar(50) not null,
+		lastname varchar(50) not null,
+		middlename varchar(50) not null,
+		constraint fk_employee foreign key(pers_id) references employee(emp_id)
+);      
+
+create table decane (
+	  id varchar(20) NOT NULL,
+	  firstname varchar(30) NOT NULL DEFAULT '',
+	  lastname varchar(30) NOT NULL DEFAULT '',
+	  faculty varchar(50) NOT NULL DEFAULT '',
+	  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-insert into decane(id,first_name,last_name,faculty) values ('rff1','valeriy','dolgov','rff');
-insert into decane(id,first_name,last_name,faculty) values ('fti1','artur','pirozhkov','fti');
+insert into decane(id,firstname,lastname,faculty) values ('rff1','valeriy','dolgov','rff');
+insert into decane(id,firstname,lastname,faculty) values ('fti1','artur','pirozhkov','fti');
 
