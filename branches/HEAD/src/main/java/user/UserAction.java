970f45a27d231d280c8d09566b47fd33ae28837a
sergei.bardas <sergei.bardas@cb2d3c85-9e30-58f3-8e0a-dd9ac8f198df>
2012-03-13 15:09:08 +0000

    Issue 15:edit user,adding Person info

--- branches/HEAD/src/main/java/user/UserAction.java
+++ branches/HEAD/src/main/java/user/UserAction.java
@@ -6,11 +6,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
-import org.hibernate.Query;
-import org.hibernate.SessionFactory;
-import org.hibernate.classic.Session;
-
 import service.UserManager;
+import entry.Employee;
+import entry.Person;
 import entry.User;
 
 @ManagedBean
@@ -19,6 +17,10 @@ public class UserAction {
 
 	@ManagedProperty(value = "#{user}")
 	private User user;
+	@ManagedProperty(value = "#{person}")
+	private Person person;
+	@ManagedProperty(value = "#{employee}")
+	private Employee employee;
 	@ManagedProperty(value = "#{userManager}")
 	private UserManager userManager;
 
@@ -28,6 +30,22 @@ public class UserAction {
 
 	public void setUserManager(UserManager userManager) {
 		this.userManager = userManager;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
 	}
 
 	public User getUser() {
@@ -45,13 +63,17 @@ public class UserAction {
 	public List<User> getUsers() {
 		return userManager.getUsers();
 	}
-	
+
 	public String editUserAction(User user) {
 		user.setEditable(true);
+		// this.user=user;
 		return null;
 	}
 
-	public int editUser(User user) {
-		return userManager.editUser(user);
+	public int editUser() {
+		// long id = (Long)
+		// e.getComponent().getAttributes().get("targetObject");
+		// User user = userManager.getUserById(id);
+		return userManager.editUser(user, person);
 	}
 }
