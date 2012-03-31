e0b96a18b848abf4c5478c44bbc1e53b97d9e25f
snum.roman@gmail.com <snum.roman@gmail.com@cb2d3c85-9e30-58f3-8e0a-dd9ac8f198df>
2012-03-31 21:10:25 +0000

    Fix problem with active tab after refresh page

--- src/main/java/web/MainBean.java
+++ src/main/java/web/MainBean.java
@@ -1,14 +1,37 @@
 package web;
 
+import java.util.Map;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.component.tabview.TabView;
+import org.primefaces.event.TabChangeEvent;
 
 @ManagedBean(name = "mainbean")
 @SessionScoped
 public class MainBean {
 
+    private int activeTabIndex = 0;
+
     public String doAction(String action) {
         return action;
     }
 
+    public void onTabChange(TabChangeEvent event) {
+        TabView tabView = (TabView) event.getComponent();
+        FacesContext context = FacesContext.getCurrentInstance();
+        Map<String, String> params = context.getExternalContext().getRequestParameterMap();
+        params.get(tabView.getClientId(context) + "_activeIndex");
+
+    }
+
+    public int getActiveTabIndex() {
+        return activeTabIndex;
+    }
+
+    public void setActiveTabIndex(int activeTabIndex) {
+        this.activeTabIndex = activeTabIndex;
+    }
 }
